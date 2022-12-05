e91e571a2fe8a798418b40238e87c7fb14e3054a
Danny Mexen <danny@arcariusmexen.com>
2022-12-05 17:51:49 +0200

    listing 2.1 - fixed scope issue

--- building-blocks/geometry.ex
+++ building-blocks/geometry.ex
@@ -2,12 +2,12 @@ defmodule Geometry do
   def rectangle_area(a, b) do
     a * b
   end
+
+  def square_area(a) do
+    rectangle_area(a, a)
+  end
 end
 
 defmodule GeometryCondensed do
   def rectangle_area(a, b), do: a * b
-end
-
-def square_area(a) do
-  rectangle_area(a, a)
 end
