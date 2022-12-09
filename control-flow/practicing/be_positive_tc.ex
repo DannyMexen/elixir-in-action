f92b0f392757b90bef53f6812be100296475187c
Danny Mexen <danny@arcariusmexen.com>
2022-12-09 21:48:57 +0200

    FAILURE

---
+++ control-flow/practicing/be_positive_tc.ex
@@ -0,0 +1,15 @@
+defmodule Be do
+
+  def positive(list) do
+    do_positive([], list)
+  end
+
+  defp do_positive([], []) do
+    []
+  end
+
+  defp do_positive([head | tail], ) when head > 0 do
+    
+
+  end
+end
