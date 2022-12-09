3a2d55ffbcddd010668d3ff898b63e833cd89f7e
Danny Mexen <danny@arcariusmexen.com>
2022-12-09 21:37:35 +0200

    added tail recurisve empty module

--- control-flow/practicing/be_positive.ex
+++ control-flow/practicing/be_positive.ex
@@ -1,13 +1,2 @@
 defmodule Be do
-
-  def positive([]), do: []
-
-  def positive([head | tail]) when head < 0 do
-   positive(tail)
-  end
-
-  def positive([head | tail]) when head > 0 do
-    [head | positive(tail)]
-  end
-
 end
