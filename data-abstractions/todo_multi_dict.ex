8e954d8bcf5acd104bd5c1583e585136e86f8a4f
Danny Mexen <danny@arcariusmexen.com>
2022-12-13 17:46:39 +0200

    Following the book. BY THE BOOK

--- data-abstractions/todo_multi_dict.ex
+++ data-abstractions/todo_multi_dict.ex
@@ -1,11 +1,11 @@
-defmodule MultiDict do
- def new(), do: %{}
+defmodule TodoList do
+  def new(), do: MultiDict.new()
 
- def add(dict, key, value) do
-   Map.update(dict, key, [value], &[value | &1])
- end
+  def add_entry(todo_list, entry) do
+    MultiDict.add(todo_list, entry.date, entry)
+  end
 
- def get(dict, key) do
-   Map.get(dict, key, [])
- end
+  def entries(todo_list, date) do
+    MultiDict.get(todo_list, date)
+  end
 end
