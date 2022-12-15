57f2ccc3400cfa06df8923e199a69e3a22fd4e55
Daniel Mwaba Mwale <dannymexen@gmail.com>
2022-12-15 12:13:03 +0200

    Listing 4.14 Updating an entry

--- data-abstractions/todo_crud.ex
+++ data-abstractions/todo_crud.ex
@@ -16,6 +16,18 @@ defmodule TodoList do
     %TodoList{todo_list | entries: new_entries, auto_id: todo_list.auto_id + 1}
   end
 
+  def update_entry(todo_list, entry_id, updater_fun) do
+    case Map.fetch(todo_list.entries, entry_id) do
+      :error ->
+        todo_list
+
+      {:ok, old_entry} ->
+        new_entry = updater_fun.(old_entry)
+        new_entries = Map.put(todo_list.entries, new_entry.id, new_entry)
+        %TodoList{todo_list | entries: new_entries}
+    end
+  end
+
   def entries(todo_list, date) do
     todo_list.entries
     |> Stream.filter(fn {_, entry} -> entry.date == date end)
