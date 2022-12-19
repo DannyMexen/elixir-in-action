214cacec3e3854fa1841e30a55cdac460b78524a
Danny Mexen <danny@arcariusmexen.com>
2022-12-19 16:38:22 +0200

    Listing 5.11 The add_entry request

--- concurrency_primitives/todo_server.ex
+++ concurrency_primitives/todo_server.ex
@@ -4,6 +4,10 @@ defmodule TodoServer do
     spawn(fn -> loop(TodoList.new()) end)
   end
 
+  def add_entry(todo_server, new_entry) do
+    send(todo_server, {:add_entry, new_entry})
+  end
+
   def loop(todo_list) do
     new_todo_list =
       receive do
@@ -12,6 +16,10 @@ defmodule TodoServer do
 
     loop(new_todo_list)
   end
+end
+
+defp process_message(todo_list, {:add_entry, new_entry}) do
+  TodoList.add_entry(todo_list, new_entry)
 end
 
 defmodule TodoList do
