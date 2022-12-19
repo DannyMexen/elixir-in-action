0e8624d8af2cad705b0a5e29274b2b6282ec9964
Danny Mexen <danny@arcariusmexen.com>
2022-12-19 16:33:29 +0200

    Listing 5.10 TodoServer basic structure

--- concurrency_primitives/todo_server.ex
+++ concurrency_primitives/todo_server.ex
@@ -1,7 +1,18 @@
 defmodule TodoServer do
+  def start do
+    # Uses a to-do list as the initial state
+    spawn(fn -> loop(TodoList.new()) end)
+  end
 
+  def loop(todo_list) do
+    new_todo_list =
+      receive do
+        message -> process_message(todo_list, message)
+      end
+
+    loop(new_todo_list)
+  end
 end
 
 defmodule TodoList do
-  
 end
