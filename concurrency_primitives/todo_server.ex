43d2a6981795c3872851eba7291bb0b09121ab7b
Danny Mexen <danny@arcariusmexen.com>
2022-12-19 16:42:06 +0200

    Listing 5.12 the entries request

--- concurrency_primitives/todo_server.ex
+++ concurrency_primitives/todo_server.ex
@@ -8,6 +8,16 @@ defmodule TodoServer do
     send(todo_server, {:add_entry, new_entry})
   end
 
+  def entries(todo_server, date) do
+    send(todo_server, {:entries, self(), date})
+
+    receive do
+      {:todo_entries, entries} -> entries
+    after
+      5000 -> {:error, :timeout}
+    end
+  end
+
   def loop(todo_list) do
     new_todo_list =
       receive do
@@ -20,6 +30,11 @@ end
 
 defp process_message(todo_list, {:add_entry, new_entry}) do
   TodoList.add_entry(todo_list, new_entry)
+end
+
+defp process_message(todo_list, {:entries, caller, date}) do
+  send(caller, {:todo_entries, TodoList.entries(todo_list, date)})
+  todo_list
 end
 
 defmodule TodoList do
