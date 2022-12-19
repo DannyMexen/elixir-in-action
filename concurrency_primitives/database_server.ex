668e1969fbf8335a041865d722aeb58b32c7d336
Danny Mexen <danny@arcariusmexen.com>
2022-12-19 10:45:25 +0200

    Listing 5.3 Implementation of run_async/2

--- concurrency_primitives/database_server.ex
+++ concurrency_primitives/database_server.ex
@@ -12,6 +12,10 @@ defmodule DatabaseServer do
     loop()
   end
 
+  def run_async(server_pid, query_def) do
+    send(server_pid, {:run_query, self(), query_def})
+  end
+
   defp run_query(query_def) do
     Process.sleep(2000)
     "#{query_def} result"
