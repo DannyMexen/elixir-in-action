740271dc9ad85a858373834ce9e35cdb817ee538
Danny Mexen <danny@arcariusmexen.com>
2022-12-20 20:20:31 +0200

    Listing 7.7 Database process

---
+++ building-a-concurrent-system/persistable_todo_cache/test/todo_cache_test.exs
@@ -0,0 +1,24 @@
+defmodule TodoCacheTest do
+  # Prepares the module for testing
+  use ExUnit.Case
+
+  # Defines a test
+  test "server_process" do
+    {:ok, cache} = Todo.Cache.start()
+    bob_pid = Todo.Cache.server_process(cache, "bob")
+
+    # Test assertions
+    assert bob_pid != Todo.Cache.server_process(cache, "alice")
+    assert bob_pid == Todo.Cache.server_process(cache, "bob")
+  end
+
+  test "to-do operations" do
+    {:ok, cache} = Todo.Cache.start()
+    alice = Todo.Cache.server_process(cache, "alice")
+    Todo.Server.add_entry(alice, %{date: ~D[2018-12-19], title: "Dentist"})
+    entries = Todo.Server.entries(alice, ~D[2018-12-19])
+
+    # Asserts a matching expression
+    assert [%{date: ~D[2018-12-19], title: "Dentist"}] = entries
+  end
+end
