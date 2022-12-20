1f6b0d06d564631a0b6d43f52ee7073587db701e
Danny Mexen <danny@arcariusmexen.com>
2022-12-20 19:57:52 +0200

    Testing to-do server operations

--- building-a-concurrent-system/todo_cache/test/todo_cache_test.exs
+++ building-a-concurrent-system/todo_cache/test/todo_cache_test.exs
@@ -6,9 +6,19 @@ defmodule TodoCacheTest do
   test "server_process" do
     {:ok, cache} = Todo.Cache.start()
     bob_pid = Todo.Cache.server_process(cache, "bob")
+
+    # Test assertions
+    assert bob_pid != Todo.Cache.server_process(cache, "alice")
+    assert bob_pid == Todo.Cache.server_process(cache, "bob")
   end
 
-  # Test assertions
-  assert bob_pid != Todo.Cache.server_process(cache, "alice")
-  assert bob_pid == Todo.Cache.server_process(cache, "bob")
+  test "to-do operations" do
+    {:ok, cache} = Todo.Cache.start()
+    alice = Todo.Cache.server_process(cache, "alice")
+    Todo.Server.add_entry(alice, %{date: ~D[2018-12-19], title: "Dentist"})
+    entries = Todo.Server.entries(alice, ~D[2018-12-19])
+
+    # Asserts a matching expression
+    assert [%{date: ~D[2018-12-19], title: "Dentist"}] = entries
+  end
 end
