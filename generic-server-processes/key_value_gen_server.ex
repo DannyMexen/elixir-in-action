71002fc31b9a4e63fc0bca724b58505dadd6ce16
Danny Mexen <danny@arcariusmexen.com>
2022-12-20 14:10:15 +0200

    Implementing GenServer callbacks

---
+++ generic-server-processes/key_value_gen_server.ex
@@ -0,0 +1,22 @@
+defmodule KeyValueStore do
+  use GenServer
+
+  # init/1 accepts one argument. This is the second argument provided to GenServer.start/2
+  # and you can use it to pass data to the server process while starting it
+  def init(_) do
+    # The result of init/1 must be in the format {:ok, initial_state}
+    {:ok, %{}}
+  end
+
+  # handle_cast/2 accepts the request and the state and should return the result
+  # in the format {:noreply, new_state}
+  def handle_cast({:put, key, value}, state) do
+    {:noreply, Map.put(state, key, value)}
+  end
+
+  # handle_call/3 takes the request, the caller information, and the state.
+  # It should return the result in the format {:reply, response, new_state}
+  def handle_call({:get, key}, _, state) do
+    {:reply, Map.get(state, key), state}
+  end
+end
