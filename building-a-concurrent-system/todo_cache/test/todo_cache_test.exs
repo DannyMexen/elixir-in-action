7b57a1688dfbdf0bbda71d49beb6d1137b7c8e8a
Danny Mexen <danny@arcariusmexen.com>
2022-12-20 17:54:48 +0200

    Listing 7.5 Testing server_process

--- building-a-concurrent-system/todo_cache/test/todo_cache_test.exs
+++ building-a-concurrent-system/todo_cache/test/todo_cache_test.exs
@@ -1,4 +1,14 @@
 defmodule TodoCacheTest do
   # Prepares the module for testing
   use ExUnit.Case
+
+  # Defines a test
+  test "server_process" do
+    {:ok, cache} = Todo.Cache.start()
+    bob_pid = Todo.Cache.server_process(cache, "bob")
+  end
+
+  # Test assertions
+  assert bob_pid != Todo.Cache.server_process(cache, "alice")
+  assert bob_pid == Todo.Cache.server_process(cache, "bob")
 end
