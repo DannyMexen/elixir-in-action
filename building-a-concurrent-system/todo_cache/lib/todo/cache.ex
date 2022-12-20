fe27aeb6dadc2f823c968517fe7cc2280f893a0c
Danny Mexen <danny@arcariusmexen.com>
2022-12-20 17:35:16 +0200

    Listing 7.2 Handling the server_process request

--- building-a-concurrent-system/todo_cache/lib/todo/cache.ex
+++ building-a-concurrent-system/todo_cache/lib/todo/cache.ex
@@ -4,4 +4,23 @@ defmodule Todo.Cache do
   def init(_) do
     {:ok, %{}}
   end
+
+  def handle_call({:server_process, todo_list_name}, _, todo_servers) do
+    case Map.fetch(todo_servers, todo_list_name) do
+      # Server exists in the map
+      {:ok, todo_server} ->
+        {:reply, todo_server, todo_servers}
+
+      # Server doesn't exist
+      :error ->
+        # Starts the new server
+        {:ok, new_server} = Todo.Server.start()
+
+        {
+          :reply,
+          new_server,
+          Map.put(todo_servers, todo_list_name, new_server)
+        }
+    end
+  end
 end
