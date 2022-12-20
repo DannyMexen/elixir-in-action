f73e5d32d83d09a358fe2402d593bcdd05f67021
Danny Mexen <danny@arcariusmexen.com>
2022-12-20 12:38:47 +0200

    Listing 6.2 Handling messages in the server process

--- generic-server-processes/server_process.ex
+++ generic-server-processes/server_process.ex
@@ -1,8 +1,24 @@
 defmodule ServerProcess do
   def start(callback_module) do
     spawn(fn ->
-      initial_state = callback_module.init() # Invokes the callback to initialize the state
+      # Invokes the callback to initialize the state
+      initial_state = callback_module.init()
       loop(callback_module, initial_state)
     end)
+  end
+
+  defp loop(callback_module, current_state) do
+    receive do
+      {:request, caller} ->
+        {response, new_state} =
+          callback_module.handle_call(
+            request,
+            current_state
+          )
+
+        send(caller, {:response, response})
+
+        loop(callback_module, new_state)
+    end
   end
 end
